f333afc76af89cc31b75ea5f13c80fa564243bf7
Instant-Monkey <sylvain.laugier.net@gmail.com>
2016-09-24 00:54:53 +0200

    il bouge bien ce serpent

--- app.js
+++ app.js
@@ -9,25 +9,37 @@ $(document).ready(function() {
     var keyMap = {left: 37, up: 38, right: 39, down: 40, space: 32 } ;
     switch (event.keyCode) {
       case keyMap.left:
+      	if (snake.direction() != "r") { 
+
         snake.changeDirection("l");
+        }
         break;
       case keyMap.right:
-        snake.changeDirection("r");    
+      	if (snake.direction() != "l") { 
+        snake.changeDirection("r");   
+        } 
         break;
       case keyMap.down:
-        snake.changeDirection("d");
+      	if (snake.direction() != "u") { 
+        	snake.changeDirection("d");
+    	}
         break;
       case keyMap.up:
-        snake.changeDirection("u");    
+     	 if (snake.direction() != "d") { 
+        	snake.changeDirection("u");   
+        } 
         break;
+       case keyMap.space:
+       clearInterval(refreshIntervalId);
+       break;
     }
   });
 
-	setInterval(function(){ 
+	refreshIntervalId = setInterval(function(){ 
 
 		moveSnake(snake.direction());
 
-	}, 1000);
+	}, 200);
   
   });
   
@@ -37,16 +49,16 @@ $(document).ready(function() {
 		this.sizeX = sizeX;
 		this.sizeY = sizeY;
 		
-		var slotEmplacement = new Array(sizeX);
-		for (var i = 0; i < sizeX; i++) {
-  			slotEmplacement[i] = new Array(sizeY);
+		var slotEmplacement = new Array(sizeY);
+		for (var i = 0; i < sizeY; i++) {
+  			slotEmplacement[i] = new Array(sizeX);
 		}
 
 
 
-		for (var x = 0; x < sizeX; x++) { 
-			for (var y = 0; y < sizeY; y++ ) { 
-				slotEmplacement[x][y] = new Slot(x,y);
+		for (var y = 0; y < sizeY; y++) { 
+			for (var x = 0; x < sizeX; x++ ) { 
+				slotEmplacement[y][x] = new Slot(x,y);
 			}
 		
 		}
@@ -68,14 +80,14 @@ $(document).ready(function() {
 		var direction = initDirect ; 
 
 		
-		var snakeEmplacement = new Array(4);
-		for (var i = 0; i < 4; i++) { 
-			var y = headCoordY - i ;
-			snakeEmplacement[i] = [headCoordX,y];
+		var snakeEmplacement = new Array(10);
+		for (var i = 0; i < 10; i++) { 
+			var x = headCoordX - i ;
+			snakeEmplacement[i] = [x,headCoordY];
 		}
 
-		this.changeDirection = function(direction) { 
-			this.direction = direction ; 
+		this.changeDirection = function(directionGiven) { 
+			direction = directionGiven ; 
 		}
 
 		this.getCoordSnake = function() { 
@@ -106,44 +118,43 @@ $(document).ready(function() {
 		var snakeEmplacement = snake.getCoordSnake(); 
 		j = snakeEmplacement.length - 1 ; 
 		eraseSnakeOneCase(snakeEmplacement[j][0],snakeEmplacement[j][1]);
+		
+		for (j; j > 0; j--) { 
+
+				snakeEmplacement[j][0]= snakeEmplacement[j - 1][0] ;
+				snakeEmplacement[j][1]= snakeEmplacement[j - 1][1] ;
+				
+		}
+		
+		
 
 		switch(direction) { 
 
-			case "r": 
-
-			for (var i = 0; i < snakeEmplacement.length; i++) { 
-				snakeEmplacement[i][1] += 1 ;
-				snake.updateSnakeEmplacement(snakeEmplacement);
-			}
+			case "r":
+		
+			snakeEmplacement[0][0] += 1 ;
+			
 			break; 
 
 			case "l": 
-
-			for (var i = 0; i < snakeEmplacement.length; i++) { 
-				snakeEmplacement[i][1] -= 1 ;
-				snake.updateSnakeEmplacement(snakeEmplacement);
-			}
+				snakeEmplacement[0][0] -= 1 ;
 			break; 
 
 			case "u": 
-
-			for (var i = 0; i < snakeEmplacement.length; i++) { 
-				snakeEmplacement[i][0] -= 1 ;
-				snake.updateSnakeEmplacement(snakeEmplacement);
-			}
+				snakeEmplacement[0][1] -= 1 ;
 			break; 
 
 			case "d": 
-
-			for (var i = 0; i < snakeEmplacement.length; i++) { 
-				snakeEmplacement[i][0] += 1 ;
-				snake.updateSnakeEmplacement(snakeEmplacement);
-			}
+				snakeEmplacement[0][1] += 1 ;
 			break; 
 
 		}
-
+		checkIfLost(snakeEmplacement);
+		snake.updateSnakeEmplacement(snakeEmplacement);
 		renderSnake(snake);
+		
+
+
 
 	}
 
@@ -162,20 +173,48 @@ $(document).ready(function() {
   }
 
   function renderOneCaseSnake(x,y) { 
-  	$("#square" + x + y).css({backgroundColor: "red"});
+  	var id = $("#squareX" + x + "Y"+ y) ;
+
+  	id.css({backgroundColor: "red"});
+  	
+  	
   }
 
   function eraseSnakeOneCase(x,y) { 
-  	$("#square" + x + y).css({backgroundColor: "white"});
+  	$("#squareX" + x + "Y"+ y).css({backgroundColor: "white"});
+  }
+
+  function checkIfLost(snakeEmplacement) { 
+  	
+  		var x = snakeEmplacement[0][0];
+  		var y = snakeEmplacement[0][1];
+  		if (x > snakeGrid.sizeX || x < 0 || y > snakeGrid.sizeY || y < 0) { 
+  			displayLost();
+
+  		}
+
+  		for (var i = 1; i < snakeEmplacement.length; i++) { 
+  	
+  			if (snakeEmplacement[0][0] == snakeEmplacement[i][0] && snakeEmplacement[0][1] == snakeEmplacement[i][1]) { 
+  				displayLost();
+
+  			}
+  		}
+  	
+  }
+
+  function displayLost() { 
+  	clearInterval(refreshIntervalId);
+  			console.log("LOST");
   }
 
   function render(grid){ 
   	var squareSizeX = $("#snake__container").width() / grid.sizeX;
   	var squareSizeY = $("#snake__container").height() / grid.sizeY;
-	for (x = 0; x < grid.sizeX; x++) { 
-			for (y = 0; y < grid.sizeY; y++ ) { 
+	for (var y = 0; y < grid.sizeY; y++) { 
+			for (var x = 0; x < grid.sizeX; x++ ) { 
 
-				var $div = $("<div>", {id: "square" + x + y, class: "square"});
+				var $div = $("<div>", {id: "squareX" + x + "Y"+ y, class: "square"});
 				$("#snake__container").append($div);
 				$(".square").css({ 
 					width: squareSizeX ,
